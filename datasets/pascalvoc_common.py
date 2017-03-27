904e377a586f05916accf541f0ddda056fd9bb27
pool1892 <pool1892@users.noreply.github.com>
2017-03-27 14:58:17 +0200

    Update pascalvoc_common.py

--- datasets/pascalvoc_common.py
+++ datasets/pascalvoc_common.py
@@ -23,27 +23,11 @@ slim = tf.contrib.slim
 
 VOC_LABELS = {
     'none': (0, 'Background'),
-    'aeroplane': (1, 'Vehicle'),
-    'bicycle': (2, 'Vehicle'),
-    'bird': (3, 'Animal'),
-    'boat': (4, 'Vehicle'),
-    'bottle': (5, 'Indoor'),
-    'bus': (6, 'Vehicle'),
-    'car': (7, 'Vehicle'),
-    'cat': (8, 'Animal'),
-    'chair': (9, 'Indoor'),
-    'cow': (10, 'Animal'),
-    'diningtable': (11, 'Indoor'),
-    'dog': (12, 'Animal'),
-    'horse': (13, 'Animal'),
-    'motorbike': (14, 'Vehicle'),
-    'person': (15, 'Person'),
-    'pottedplant': (16, 'Indoor'),
-    'sheep': (17, 'Animal'),
-    'sofa': (18, 'Indoor'),
-    'train': (19, 'Vehicle'),
-    'tvmonitor': (20, 'Indoor'),
-}
+    'full_cart': (1, 'Full Cart'),
+    'empty_cart': (2, 'Empty Cart'),
+    'full_basket': (3, 'Full Basket'),
+    'empty_basket': (4, 'Empty Basket')
+ }
 
 
 def get_split(split_name, dataset_dir, file_pattern, reader,
